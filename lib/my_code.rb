29b6fcb521952de3b6cf53a703b0f29efe6efc2f
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-29 20:47:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@
 require 'pry'
 def map(source_array)
   map_array = []
-  for n in 0..sourcearray.length
+  for n in 0..source_array.length
     map_array << yield(source_array[n])
   end
   map_array
