f2b9f757c955915c7012bdb84de14d2393084c4d
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-29 21:21:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(source_array)
+  map_array = []
+  source_array.each do |num|
+    map_array << yield(num)
+  end
+  map_array
+end
+
+def reduce(source_array, starting_point = 0)
+  accumulator = starting_point
+  source_array.length.times do |i|
+   accumulator = yield(source_array[i], accumulator)
+  end
+  accumulator = accumulator == nil ? false : accumulator
+end
